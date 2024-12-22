6e3e09423abb0763b103f77d2891f921eaa2570c
Vladislav <vladivektin@mail.ru>
2024-12-22 21:32:41 +0300

    #add added function for drop database

--- database/library_init_schema.sql
+++ database/library_init_schema.sql
@@ -1,13 +1,24 @@
-DROP FUNCTION IF EXISTS online_library_init.online_library_db_init;
+DROP FUNCTION IF EXISTS online_library_init.initialize_database;
+DROP FUNCTION IF EXISTS online_library_init.drop_database;
 
 DROP SCHEMA IF EXISTS online_library_init;
 
 CREATE SCHEMA online_library_init AUTHORIZATION library_owner;
 
-CREATE OR REPLACE FUNCTION online_library_init.online_library_db_init() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION online_library_init.initialize_database() RETURNS VOID AS $$
 BEGIN
+	---
+	--- СОЗДАНИЕ СХЕМ
+	---
+
 	CREATE SCHEMA online_library_tables AUTHORIZATION library_owner; 
 	CREATE SCHEMA online_library_functional AUTHORIZATION library_owner;
+
+
+	---
+	--- СОЗДАНИЕ ТАБЛИЦ
+	---
+	
 
 	CREATE TABLE online_library_tables.user
 	(
@@ -67,6 +78,12 @@ BEGIN
 		rating numeric(4, 2) DEFAULT NULL
 	);
 
+
+	--
+	-- ДОБАВЛЕНИЕ ВНЕШНИХ КЛЮЧЕЙ
+	--
+
+
 	ALTER TABLE online_library_tables.saving
 	ADD CONSTRAINT fk_saving_user FOREIGN KEY (user_id) REFERENCES online_library_tables.user (user_id)
 		ON UPDATE CASCADE ON DELETE CASCADE;
@@ -87,14 +104,144 @@ BEGIN
 	ADD CONSTRAINT fk_book_author_author FOREIGN KEY (author_id) REFERENCES online_library_tables.author (author_id)
 		ON UPDATE CASCADE ON DELETE CASCADE;
 
+
+	---
+	--- ДОБАВЛЕНИЕ ДРУГИХ ОГРАНИЧЕНИЙ
+	---
+	
+
 	ALTER TABLE online_library_tables.saving
 	ADD CONSTRAINT unq_saving_user_id_book_id UNIQUE (user_id, book_id);
 	
 	ALTER TABLE online_library_tables.publisher
 	ADD CONSTRAINT unq_publisher_city_address UNIQUE (city, address);
 
+	
+	---
+	--- СОЗДАНИЕ ИНДЕКСОВ
+	---
+	
+
+	CREATE INDEX idx_user_email ON online_library_tables.user(email);
 	CREATE INDEX idx_book_genre_lower ON online_library_tables.book( LOWER (genre) );
-	CREATE INDEX idx_author_last_name_lower ON online_library_tables.author ( LOWER (last_name) );
-	CREATE INDEX idx_publisher_city_lower ON online_library_tables.publisher ( LOWER (city) );
+	CREATE INDEX idx_author_last_name_lower ON online_library_tables.author( LOWER (last_name) );
+
+
+	---
+	--- ВСТАВКА ТЕСТОВЫХ ДАННЫХ
+	---
+
+	INSERT INTO online_library_tables.author (first_name, last_name, patronymic)
+	VALUES
+	('Фёдор', 'Достоевский', 'Михайлович'),
+	('Михаил', 'Булгаков', 'Афанасьевич'),
+	('Джек', 'Лондон', null);
+	
+	INSERT INTO online_library_tables.publisher (publisher_name, city, address, email)
+	VALUES
+	('АСТ', 'Москва', 'Ленинская 23/11', 'ASTPublisherMoscow@inbox.ru'),
+	('Литрес', 'Санкт-Петербург', 'Яблоневая 47', 'LitresPublisherSPb@mail.ru'),
+	('XL Media', 'Екатеринбург', 'Матросова 27/3', 'XLMediaPublisherEkb@mail.ru');
+	
+	INSERT INTO online_library_tables.book (title, genre, publisher_id, publication_year)
+	VALUES
+	('Преступление и наказание', 'роман', 1, 2017),
+	('Идиот', 'роман', 1, 2017),
+	('Униженные и оскорбленные', 'роман', 2, 2018),
+	('Мастер и маргарита', 'роман', 1, 2015),
+	('Морфий', 'рассказ', 3, 2023),
+	('Мартин Иден', 'роман', 1, 2018),
+	('Странник по звездам', 'роман', 2, 2020);
+	
+	INSERT INTO online_library_tables.book_author VALUES
+	(1, 1),
+	(2, 1),
+	(3, 1),
+	(4, 2),
+	(5, 2),
+	(6, 3),
+	(7, 3);
 END
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION online_library_init.drop_database() RETURNS VOID AS $$
+BEGIN
+	---
+	--- ОЧИСТКА ТРИГГЕРОВ
+	---
+
+	DROP TRIGGER IF EXISTS trg_set_saving_date ON online_library_tables.saving;
+	DROP TRIGGER IF EXISTS trg_update_book_rating ON online_library_tables.saving;
+	DROP TRIGGER IF EXISTS trg_update_author_rating ON online_library_tables.book;
+	DROP TRIGGER IF EXISTS trg_delete_books_after_author_delete ON online_library_tables.author;
+
+	---
+	--- ОЧИСТКА ФУНКЦИЙ
+	---
+	
+	
+	DROP FUNCTION IF EXISTS online_library_functional.track_saving_data;
+	DROP FUNCTION IF EXISTS online_library_functional.update_book_rating;
+	DROP FUNCTION IF EXISTS online_library_functional.update_author_rating;
+	DROP FUNCTION IF EXISTS online_library_functional.delete_books_after_author_delete;
+	DROP FUNCTION IF EXISTS online_library_functional.get_all_data_from_tables;
+	DROP FUNCTION IF EXISTS online_library_functional.get_all_data_from_table_by_table_name;
+	DROP FUNCTION IF EXISTS online_library_functional.truncate_table_by_name;
+	DROP FUNCTION IF EXISTS online_library_functional.truncate_all_tables();
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_user;
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_saving;
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_book;
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_publisher;
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_book_author;
+	DROP FUNCTION IF EXISTS online_library_functional.insert_into_table_author;
+	DROP FUNCTION IF EXISTS online_library_functional.update_phone_into_table_user;
+	DROP FUNCTION IF EXISTS online_library_functional.update_email_into_table_user;
+	DROP FUNCTION IF EXISTS online_library_functional.set_patronymic_into_table_user;
+	DROP FUNCTION IF EXISTS online_library_functional.set_has_read_into_table_saving;
+	DROP FUNCTION IF EXISTS online_library_functional.set_rating_into_table_saving;
+	DROP FUNCTION IF EXISTS online_library_functional.update_into_table_book;
+	DROP FUNCTION IF EXISTS online_library_functional.update_into_table_publisher;
+	DROP FUNCTION IF EXISTS online_library_functional.update_into_table_book_author;
+	DROP FUNCTION IF EXISTS online_library_functional.update_into_table_author;
+	DROP FUNCTION IF EXISTS online_library_functional.find_row_by_id;
+	DROP FUNCTION IF EXISTS online_library_functional.search_books_by_genre;
+	DROP FUNCTION IF EXISTS online_library_functional.search_books_by_title;
+	DROP FUNCTION IF EXISTS online_library_functional.search_books_by_author_nsp;
+	DROP FUNCTION IF EXISTS online_library_functional.search_books_by_publisher_name;
+	DROP FUNCTION IF EXISTS online_library_functional.search_authors_by_author_nsp;
+	DROP FUNCTION IF EXISTS online_library_functional.search_publishers_by_name;
+	DROP FUNCTION IF EXISTS online_library_functional.search_publishers_by_city;
+	DROP FUNCTION IF EXISTS online_library_functional.search_users_by_user_nsp;
+	DROP FUNCTION IF EXISTS online_library_functional.delete_row_by_id;
+	DROP FUNCTION IF EXISTS online_library_functional.delete_author_by_author_nsp;
+	DROP FUNCTION IF EXISTS online_library_functional.delete_publisher_by_name;
+	DROP FUNCTION IF EXISTS online_library_functional.delete_user_by_user_nsp;
+	DROP FUNCTION IF EXISTS online_library_functional.search_password_and_is_admin_by_user_email;
+
+
+	--
+	-- ОЧИЩЕНИЕ ТАБЛИЦ
+	--
+	
+	
+	DROP TABLE IF EXISTS online_library_tables.user CASCADE;
+	DROP TABLE IF EXISTS online_library_tables.saving CASCADE;
+	DROP TABLE IF EXISTS online_library_tables.book CASCADE;
+	DROP TABLE IF EXISTS online_library_tables.publisher CASCADE;
+	DROP TABLE IF EXISTS online_library_tables.book_author CASCADE;
+	DROP TABLE IF EXISTS online_library_tables.author CASCADE;
+
+
+	--
+	-- ОЧИЩЕНИЕ СХЕМ
+	--
+
+
+	DROP SCHEMA IF EXISTS online_library_tables;
+	DROP SCHEMA IF EXISTS online_library_functional;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT 
+
